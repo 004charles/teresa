fc504322123bd8eab7d40fca168800d7fa71c83e
muquissi <muquissi@carlos>
2024-08-09 03:51:47 -0300

    ola

--- projecto_venda/urls.py
+++ projecto_venda/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('', views.index, name= 'index' ),
     path('vetor5/', include('usuarios.urls')),
 
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
